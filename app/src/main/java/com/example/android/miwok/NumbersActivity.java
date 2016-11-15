88f244a9b21baf8452a377eda9fe21f47a5bd464
Zarkwelder <osborne.andrewj@gmail.com>
2016-11-15 15:32:49 -0800

    Adding visual polish

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,15 +17,15 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayList<Word> wordsList = new ArrayList<>();
 
         wordsList.add(new Word("one", "lutti", R.drawable.number_one));
-        wordsList.add(new Word("two", "otiiko"));
-        wordsList.add(new Word("three", "tolookosu"));
-        wordsList.add(new Word("four", "oyyisa"));
-        wordsList.add(new Word("five", "massokka"));
-        wordsList.add(new Word("six", "temmokka"));
-        wordsList.add(new Word("seven", "kenekaku"));
-        wordsList.add(new Word("eight", "kawinta"));
-        wordsList.add(new Word("nine", "wo'e"));
-        wordsList.add(new Word("ten", "na'aacha"));
+        wordsList.add(new Word("two", "otiiko", R.drawable.number_two));
+        wordsList.add(new Word("three", "tolookosu", R.drawable.number_three));
+        wordsList.add(new Word("four", "oyyisa", R.drawable.number_four));
+        wordsList.add(new Word("five", "massokka", R.drawable.number_five));
+        wordsList.add(new Word("six", "temmokka", R.drawable.number_six));
+        wordsList.add(new Word("seven", "kenekaku", R.drawable.number_seven));
+        wordsList.add(new Word("eight", "kawinta", R.drawable.number_eight));
+        wordsList.add(new Word("nine", "wo'e", R.drawable.number_nine));
+        wordsList.add(new Word("ten", "na'aacha", R.drawable.number_ten));
 
         WordAdapter itemsAdapter = new WordAdapter(this, R.layout.list_item, wordsList);
 
